6167b2410b28daa0ba8d8401907cbecaccd5b9cb
DROUSSI <imrane.a.droussi@capgemini.com>
2019-04-16 09:12:03 +0100

    testing

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace WebApplication1.Controllers
         // GET: Home
         public string Index()
         {
-            return "Hello world entreprise";
+            return "Hello world Imrane entreprise";
         }
     }
 }
